f390403158eabd6b74cd0fdd142f06007b6eab51
Raphael <raphael.mazet@gmail.com>
2023-06-02 14:37:54 +0100

    Commented out the subgraph menu button

--- src/Components/Navigation/NavBar/index.tsx
+++ src/Components/Navigation/NavBar/index.tsx
@@ -60,6 +60,7 @@ export interface INavBarProps {
                         transition: 'all 0.2s ease-in-out',
                       }}
                     >API Documentation</Button>
+                   { /*
                     <Button 
                       onClick={() => window.open("https://subgraphs.docs.simplefi.finance/subgraph-development-documentation/dashboard-integration", "_blank")}
                       isActive={location.pathname === '/subgraphs/documentation'} 
@@ -70,6 +71,7 @@ export interface INavBarProps {
                         transition: 'all 0.2s ease-in-out',
                       }}
                     >Subgraphs Documentation</Button>
+                    */}
                     <Button 
                       onClick={() => navigate('/#subscribe')} 
                       _active={{
