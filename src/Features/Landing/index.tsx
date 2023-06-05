8db66dabc6dd13329583d00548d753b99bf2d5a9
Raphael <raphael.mazet@gmail.com>
2023-06-05 16:25:06 +0100

    Update all copy on landing

--- src/Features/Landing/index.tsx
+++ src/Features/Landing/index.tsx
@@ -154,8 +154,8 @@ export const Landing = () => {
                     <CardHeader
                         py={useBreakpointValue({ base: '6', md: '4' })}
                     >
-                        <Heading as="h1" size={useBreakpointValue({ sm: 'xs', md: 'xs', base: 'xs' })} color="#bf1d82">Dashboard</Heading>
-                        <Heading  as='h2' size={useBreakpointValue({ sm: 'sm', md: 'sm', base: 'sm'})} py={{ base: '2', md: '4' }}>Actionable data</Heading>
+                        <Heading as="h1" size={useBreakpointValue({ sm: 'xs', md: 'xs', base: 'xs' })} color="#bf1d82">Explore</Heading>
+                        <Heading  as='h2' size={useBreakpointValue({ sm: 'sm', md: 'sm', base: 'sm'})} py={{ base: '2', md: '4' }}>SimpleFi your data</Heading>
                     </CardHeader>
                     <Card direction={useBreakpointValue({ md: 'row', sm: 'column'})} bg="transparent" border="none" boxShadow="none" justify="space-evenly" align="flex-start">
                         <Card bg="transparent" border="none" boxShadow="none" p={useBreakpointValue({ base: '2', md: '4' })} justify="center" width={useBreakpointValue({base: '100%', md: '45%'})}>
@@ -173,10 +173,10 @@ export const Landing = () => {
                             <Heading 
                                 size={useBreakpointValue({ base: 'xs', md: 'xs', lg: 'md' })}
                                 paddingY={4}>
-                                    <span style={{ color:"#bf1d82"}}>Track</span> your ROI
+                                    <span style={{ color:"#bf1d82"}}>Unified</span> API
                             </Heading>
                             <Text>
-                            Instantly see the profits (or losses) from all your investments in DeFi liquidity pools, yield farms, lending pools and more.
+                            Query our endpoints for comprehensive unified data on all DeFi categories, including Dexes, borrowing protocols, staking protocols, perp protocols, etc.
                             </Text>
                         </Card>
                         {!isDesktop && <Spacer />}
@@ -194,10 +194,10 @@ export const Landing = () => {
                             <Heading 
                                 size={useBreakpointValue({ base: 'xs', md: 'xs', lg: 'md' })}
                                 paddingY={4}>
-                                    <span style={{ color:"#bf1d82"}}>Boost</span> your Portfolio
+                                    <span style={{ color:"#bf1d82"}}>Modular</span> engine
                             </Heading>
                             <Text>
-                            Discover ways to increase your returns and find new investment opportunities thanks to our yield optimization tools.
+                            Self-host your data thanks to our powerful, modular engine. Easily choose which chains and protocols to index and analyze. Bootstrap your database in an instant.
                             </Text>
                         </Card>
                     </Card>
@@ -216,14 +216,14 @@ export const Landing = () => {
                         px={useBreakpointValue({ base: '2', md: '4' })}
                         pb={1}
                     >
-                        <Heading as="h1" size={useBreakpointValue({ sm: 'xs', md: 'xs', base: 'xs' })} color="#bf1d82">Api</Heading>
-                        <Heading as="h2" size={useBreakpointValue({ sm: 'sm', md: 'sm', base: 'xs'})} py={{ base: '2', md: '4' }} color="white">Multichain data API</Heading>
+                        <Heading as="h1" size={useBreakpointValue({ sm: 'xs', md: 'xs', base: 'xs' })} color="#bf1d82">Contribute</Heading>
+                        <Heading as="h2" size={useBreakpointValue({ sm: 'sm', md: 'sm', base: 'xs'})} py={{ base: '2', md: '4' }} color="white">Join the open data revolution</Heading>
                     </CardHeader>
                     <CardBody
                         px={useBreakpointValue({ base: '2', md: '4' })}
                     >
                         <Text color="white" fontSize={useBreakpointValue({ sm: 'md', md: 'large', base: 'md' })} pb={2}>
-                            Our API covers <span style={{color: '#f636ad'}}>10</span> chains and serves <span style={{color: '#f636ad'}}>1,578,345,954</span> DeFi data points including pricing, performance and optimization analytics
+                            DeFi data should be accessible, uncensored and permissionless... <span style={{color: '#f636ad'}}>just like the protocols themselves.</span> SimpleFi is fully open source, and provides <span style={{color: '#f636ad'}}>DeFi analytics as a public good. </span>Join us!
                         </Text>
                         <Card direction={{ base: 'column', md: 'row', sm: 'column'}} bg="transparent" border="none" boxShadow="none" justify="space-between">
                             <Card bg="transparent" border="none" boxShadow="none" justify="center" width={useBreakpointValue({ sm: '100%', md: '45%', base: '100%' })} px={1}>
@@ -232,10 +232,10 @@ export const Landing = () => {
                                     size={useBreakpointValue({ sm: 'sm', md: 'xs', base: 'xs'})}
                                     paddingY={4}
                                     color="white"
-                                > Account Data
+                                > Anyone can contribute
                                 </Heading>
                                 <Text fontSize={useBreakpointValue({ sm: 'md', md: 'large', base: 'md' })}  color="white">
-                                Analytics for every DeFi account, including current and historical portfolio positions, ROI and APY, idle and invested assets and portfolio optimization insights.
+                                Our data and analytics pipelines are entirely coded in Rust. Visit our github and help us make the platform even more powerful.
                                 </Text>
                             </Card>
                             {!isDesktop && <Spacer />}
@@ -246,20 +246,17 @@ export const Landing = () => {
                                     paddingY={4}
                                     color="white"
                                 > 
-                                    Market Data
+                                    Decentralized network
                                 </Heading>
                                 <Text fontSize={useBreakpointValue({ sm: 'md', md: 'large', base: 'md' })} color="white">
-                                    General market-level analytics for all DeFi protocols, including pricing for 100k+ tokens (incl. LP tokens), pool ROI and APY, volumetric data, and much more.
+                                    In the age of AI, giving up control to private companies is a bad idea. Help build the network that serves training data as a public good.
                                 </Text>
                             </Card>
                         </Card>
                     </CardBody>
                         <Stack direction={{ base: 'column', md: 'row' }} spacing={{ base: '3', md: '6' }} marginTop={5}>
                             <Button variant="primary" size={useBreakpointValue({ base: 'md', md: 'md' })} onClick={() => navigate('/api/documentation')}>
-                                API Documentation
-                            </Button>
-                            <Button variant="primary" size={useBreakpointValue({ base: 'md', md: 'md' })} onClick={() => window.open('https://forms.gle/pa4LonrFE63A2ygU7', '_blank')}>
-                                Get API Key
+                                Contribute now
                             </Button>
                         </Stack>
                 </Card>
